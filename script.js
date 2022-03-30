083bc86dc9a4e1c1418ee30ac07e2c8a5f99c080
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-03-30 21:42:42 +0100

    add async code

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const url = "https://geo.ipify.org/api/v2/country?apiKey=";
+const geoApiKey = "at_bfYP0ALvxe1bi1y15ZvlujMFc84Ap";
+const button = document.querySelector("button");
+const input = document.querySelector("#search");
 
+const getIP = async (domain, input) => {
+  const endPoint = `${url}${geoApiKey}${domain}${input}`;
+  const response = await fetch(endPoint);
+  const jsonResponse = await response.json();
+  if (response.ok) {
+    let ipAddress = (document.querySelector(".ip__address").innerText =
+      jsonResponse.ip);
+    let location = (document.querySelector(
+      ".location"
+    ).innerText = `${jsonResponse.location.region}, ${jsonResponse.location.country}`);
+    let timezone = (document.querySelector(
+      ".timezone"
+    ).innerText = `UTC${jsonResponse.location.timezone}`);
+    let isp = (document.querySelector(".isp").innerText = jsonResponse.isp);
+  }
+};
+
+getIP("", "");
+
+button.addEventListener("click", () => {
+  let newInput = input.value;
+  if (!newInput == "") {
+    getIP("&domain=", newInput);
+  }
+});
