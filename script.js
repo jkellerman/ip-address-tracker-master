d44cae1d3c52c7fec791831d2a5d6b111ef608da
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-03-31 21:47:33 +0100

    map changes on input

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ const geoApiKey = "at_bfYP0ALvxe1bi1y15ZvlujMFc84Ap";
 const button = document.querySelector("button");
 const input = document.querySelector("#search");
 
+let map = L.map("map").setView([51.505, -0.09], 15);
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoiamtlbGwiLCJhIjoiY2t3N3JlNmFzMWVnYjJxbWVxNGR5cXh4MSJ9.CCiBpULqHcrdq2fXBv7qog",
+  }
+).addTo(map);
+
+let marker = L.marker([51.505, -0.09]).addTo(map);
+
 const getIP = async (param, input) => {
+  map.remove();
   const endPoint = `${url}${geoApiKey}${param}${input}`;
   const response = await fetch(endPoint);
   const jsonResponse = await response.json();
   if (response.ok) {
-    console.log(jsonResponse);
+    let latitude = jsonResponse.location.lat;
+    let longitute = jsonResponse.location.lng;
     let ipAddress = (document.querySelector(".ip__address").innerText =
       jsonResponse.ip);
     let location = (document.querySelector(
@@ -18,9 +37,9 @@ const getIP = async (param, input) => {
       ".timezone"
     ).innerText = `UTC${jsonResponse.location.timezone}`);
     let isp = (document.querySelector(".isp").innerText = jsonResponse.isp);
-    let latitude = jsonResponse.location.lat;
-    let longitute = jsonResponse.location.lng;
-    const map = L.map("map").setView([latitude, longitute], 15);
+
+    // add map
+    map = L.map("map").setView([latitude, longitute], 15);
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
@@ -35,7 +54,7 @@ const getIP = async (param, input) => {
       }
     ).addTo(map);
 
-    const marker = L.marker([latitude, longitute]).addTo(map);
+    let marker = L.marker([latitude, longitute]).addTo(map);
   }
 };
 
